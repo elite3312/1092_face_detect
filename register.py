015c96474059f111157a6090a5e770374179f706
Perry <perrywu12@gmail.com>
2021-03-13 17:08:26 +0800

    first commit

---
+++ register.py
@@ -0,0 +1,30 @@
+import cv2
+def main(name):
+  cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+  #variable to record this is the nth picture taken
+  pictureCount=0
+
+  while(cap.isOpened()):
+    # get a frame
+    ret, frame = cap.read()
+      
+    cv2.imshow("press c to capture, then press q to finish", frame)
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+      cv2.putText(frame, 'Smile', (200, 200), cv2. FONT_HERSHEY_SIMPLEX , 2, ( 255, 255, 255), 2, cv2. LINE_AA)          
+      cv2.imshow( "press c to capture, then press q to finish", frame)
+      cv2.waitKey(1000)
+      ret, frame = cap.read()
+      cv2.imwrite("rec/pic/"+name+"#"+str(pictureCount)+".jpg", frame)
+      cv2.putText(frame, 'ok', (200, 250), cv2. FONT_HERSHEY_SIMPLEX , 2, ( 255, 255, 255), 2, cv2. LINE_AA)          
+      cv2.imshow( "press c to capture, then press q to finish", frame)
+      cv2.waitKey(300)
+      pictureCount+=1
+      if(pictureCount>=3):
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  
+      
+  cap.release()
+  cv2.destroyAllWindows()
+
